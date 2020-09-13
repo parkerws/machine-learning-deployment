19cc7af07435e54f58a3b5bd73ab687f4c86e80d
parkerws <parkerws3824@gmail.com>
2020-09-12 20:06:09 -0400

    added heroku files

--- ml_flask/main.py
+++ ml_flask/main.py
@@ -23,5 +23,5 @@ def predict():
 def test():
     return 'Pinging model application!'
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+#if __name__ == '__main__':
+#    app.run(debug=True, host='127.0.0.1', port=5000)
